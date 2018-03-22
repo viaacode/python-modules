e09a2a9b996675cae865e600c5d0d097e189c731
Mike De Smet <mike.desmet@viaa.be>
2018-03-22 15:17:22 +0100

    add a basic standfordner implementation

---
+++ stanfordner.py
@@ -0,0 +1,16 @@
+import nltk
+from nltk.tag import StanfordNERTagger
+from nltk.tokenize import word_tokenize
+import os
+dir_path = os.path.dirname(os.path.realpath(__file__)) + '/'
+
+class StanfordNER:
+    def __init__(self, path = dir_path):
+        self.ner = StanfordNERTagger(path + 'classifiers/english.all.3class.distsim.crf.ser.gz',
+                                path + 'stanford-ner.jar',
+                                encoding='utf-8')
+
+    def tokenize(self, text):
+        tokenized_text = word_tokenize(text)
+        classified_text = self.ner.tag(tokenized_text)
+        return classified_text
